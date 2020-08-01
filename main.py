89083d12283f5709d5d828dfeff3d7ed89b99c8c
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 20:57:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, MessageAction)
+    MessageEvent, TextMessage, TemplateSendMessage, ButtonsTemplate, MessageAction)
 
 
 app = Flask(__name__)
@@ -34,35 +34,33 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
-    if text == 'test':
-        line_bot_api.reply_message(event.reply_token, text)
-    # if text == 'start':
-    #     buttons_template = ButtonsTemplate(
-    #         title='どの機能を利用しますか？', actions=[
-    #             MessageAction(label='①業界について',
-    #                           text='''
-    #                           メーカー
-    #                           サービス・インフラ
-    #                           商社
-    #                           ソフトウェア
-    #                           小売
-    #                           広告・出版・マスコミ
-    #                           金融
-    #                           官公庁・公社・団体
-    #                           '''),
-    #             MessageAction(label='②就活ツールについて',
-    #                           text='test'),
-    #             MessageAction(label='③インターンについて',
-    #                           text='test'),
-    #             MessageAction(label='④OB・OG訪問について',
-    #                           text='test'),
-    #             MessageAction(label='⑤その他（感想等）',
-    #                           text='test')
-    #         ])
-    #     template_message = TemplateSendMessage(template=buttons_template)
-    #     line_bot_api.reply_message(event.reply_token, template_message)
-    # else:
-    #     pass
+    if text == 'start':
+        buttons_template = ButtonsTemplate(
+            title='どの機能を利用しますか？', actions=[
+                MessageAction(label='①業界について',
+                              text='''
+                              メーカー
+                              サービス・インフラ
+                              商社
+                              ソフトウェア
+                              小売
+                              広告・出版・マスコミ
+                              金融
+                              官公庁・公社・団体
+                              '''),
+                MessageAction(label='②就活ツールについて',
+                              text='test'),
+                MessageAction(label='③インターンについて',
+                              text='test'),
+                MessageAction(label='④OB・OG訪問について',
+                              text='test'),
+                MessageAction(label='⑤その他（感想等）',
+                              text='test')
+            ])
+        template_message = TemplateSendMessage(template=buttons_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    else:
+        pass
 
 
 if __name__ == '__main__':
