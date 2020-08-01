459dd8156b4bebca64ab4e7cca729798ff4d4de0
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 21:52:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,21 +35,17 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
-    
-    if text == 'wow':
-        line_bot_api.reply_message(event.reply_token, text)
-        
-#     if text == 'start':
-#         buttons_template = ButtonsTemplate(
-#             title='どの機能を使用しますか？', text='（下記ボタンを押してください）', actions=[
-#                 MessageAction(label='①業界について', text='test'),
-#                 MessageAction(label='②就活ツールについて', text='test'),
-#                 MessageAction(label='③インターンについて', text='test'),
-#                 MessageAction(label='④OBOG訪問について', text='test'),
-#                 MessageAction(label='⑤その他', text='test')
-#             ])
-#         template_message = TemplateSendMessage(template=buttons_template)
-#         line_bot_api.reply_message(event.reply_token, template_message)
+    if text == 'start':
+        buttons_template = ButtonsTemplate(
+            title='どの機能を使用しますか？', text='（下記ボタンを押してください）', actions=[
+                MessageAction(label='①業界について', text='test'),
+                MessageAction(label='②就活ツールについて', text='test'),
+                MessageAction(label='③インターンについて', text='test'),
+                MessageAction(label='④OBOG訪問について', text='test'),
+                MessageAction(label='⑤その他', text='test')
+            ])
+        template_message = TemplateSendMessage(template=buttons_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
 
 
 if __name__ == '__main__':
