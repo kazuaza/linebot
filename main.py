de88f00c436a7648e4571663e8b7058b46eb5170
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 21:00:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -34,33 +34,33 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
-    if text == 'start':
-        buttons_template = ButtonsTemplate(
-            title='どの機能を利用しますか？', actions=[
-                MessageAction(label='①業界について',
-                              text='''
-                              メーカー
-                              サービス・インフラ
-                              商社
-                              ソフトウェア
-                              小売
-                              広告・出版・マスコミ
-                              金融
-                              官公庁・公社・団体
-                              '''),
-                MessageAction(label='②就活ツールについて',
-                              text='test'),
-                MessageAction(label='③インターンについて',
-                              text='test'),
-                MessageAction(label='④OB・OG訪問について',
-                              text='test'),
-                MessageAction(label='⑤その他（感想等）',
-                              text='test')
-            ])
-        template_message = TemplateSendMessage(template=buttons_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
+#     if text == 'start':
+#         buttons_template = ButtonsTemplate(
+#             title='どの機能を利用しますか？', actions=[
+#                 MessageAction(label='①業界について',
+#                               text='''
+#                               メーカー
+#                               サービス・インフラ
+#                               商社
+#                               ソフトウェア
+#                               小売
+#                               広告・出版・マスコミ
+#                               金融
+#                               官公庁・公社・団体
+#                               '''),
+#                 MessageAction(label='②就活ツールについて',
+#                               text='test'),
+#                 MessageAction(label='③インターンについて',
+#                               text='test'),
+#                 MessageAction(label='④OB・OG訪問について',
+#                               text='test'),
+#                 MessageAction(label='⑤その他（感想等）',
+#                               text='test')
+#             ])
+#         template_message = TemplateSendMessage(template=buttons_template)
+#         line_bot_api.reply_message(event.reply_token, template_message)
         
-    elif text == 'carousel':
+    if text == 'carousel':
         carousel_template = CarouselTemplate(columns=[
             CarouselColumn(text='hoge1', title='fuga1', actions=[
                 URIAction(label='Go to line.me', uri='https://line.me'),
