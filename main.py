36a5233d20d0088419686fd4be7d466938b72517
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 21:53:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -37,13 +37,12 @@ def handle_message(event):
     text = event.message.text
     if text == 'start':
         buttons_template = ButtonsTemplate(
-            title='どの機能を使用しますか？', text='（下記ボタンを押してください）', actions=[
-                MessageAction(label='①業界について', text='test'),
+            title='どの機能を使用しますか？', text='（下記ボタンを押してください）', 
+            actions=[MessageAction(label='①業界について', text='test'),
                 MessageAction(label='②就活ツールについて', text='test'),
                 MessageAction(label='③インターンについて', text='test'),
                 MessageAction(label='④OBOG訪問について', text='test'),
-                MessageAction(label='⑤その他', text='test')
-            ])
+                MessageAction(label='⑤その他', text='test')])
         template_message = TemplateSendMessage(template=buttons_template)
         line_bot_api.reply_message(event.reply_token, template_message)
 
