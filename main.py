7d053acec4e8d2bc6f4b629e75cf517ca8d4bd4f
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 20:30:21 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, abort
+import os
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent, TextMessage, TemplateSendMessage, ButtonsTemplate, MessageAction
+)
+
+
+app = Flask(__name__)
+
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ['YOUR_CHANNEL_ACCESS_TOKEN']
+YOUR_CHANNEL_SECRET = os.environ['YOUR_CHANNEL_SECRET']
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info('Request body: ' + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.text == 'start':
+        buttons_template = ButtonsTemplate(
+            title='どの機能を利用しますか？',
+            actions=[
+                MessageAction(label='①業界について',
+                              text='''
+                              メーカー
+                              サービス・インフラ
+                              商社
+                              ソフトウェア
+                              小売
+                              広告・出版・マスコミ
+                              金融
+                              官公庁・公社・団体
+                              '''),
+                MessageAction(label='②就活ツールについて',
+                              text='test'),
+                MessageAction(label='③インターンについて',
+                              text='test'),
+                MessageAction(label='④OB・OG訪問について',
+                              text='test'),
+                MessageAction(label='⑤その他（感想等）',
+                              text='test')
+            ])
+        template_message = TemplateSendMessage(template=buttons_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT'))
+    app.run(host='0.0.0.0', port=port)
