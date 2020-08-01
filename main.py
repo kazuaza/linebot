d83486a45abcf5eec753bce98b6487479738d83b
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 21:03:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -48,46 +48,46 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
-#     if text == 'start':
-#         buttons_template = ButtonsTemplate(
-#             title='どの機能を利用しますか？', actions=[
-#                 MessageAction(label='①業界について',
-#                               text='''
-#                               メーカー
-#                               サービス・インフラ
-#                               商社
-#                               ソフトウェア
-#                               小売
-#                               広告・出版・マスコミ
-#                               金融
-#                               官公庁・公社・団体
-#                               '''),
-#                 MessageAction(label='②就活ツールについて',
-#                               text='test'),
-#                 MessageAction(label='③インターンについて',
-#                               text='test'),
-#                 MessageAction(label='④OB・OG訪問について',
-#                               text='test'),
-#                 MessageAction(label='⑤その他（感想等）',
-#                               text='test')
-#             ])
-#         template_message = TemplateSendMessage(template=buttons_template)
-#         line_bot_api.reply_message(event.reply_token, template_message)
-        
-    if text == 'carousel':
-        carousel_template = CarouselTemplate(columns=[
-            CarouselColumn(text='hoge1', title='fuga1', actions=[
-                URIAction(label='Go to line.me', uri='https://line.me'),
-                PostbackAction(label='ping', data='ping')
-            ]),
-            CarouselColumn(text='hoge2', title='fuga2', actions=[
-                PostbackAction(label='ping with text', data='ping', text='ping'),
-                MessageAction(label='Translate Rice', text='米')
-            ]),
-        ])
-        template_message = TemplateSendMessage(
-            alt_text='Carousel alt text', template=carousel_template)
+    if text == 'start':
+        buttons_template = ButtonsTemplate(
+            title='どの機能を利用しますか？', actions=[
+                MessageAction(label='①業界について',
+                              text='''
+                              メーカー
+                              サービス・インフラ
+                              商社
+                              ソフトウェア
+                              小売
+                              広告・出版・マスコミ
+                              金融
+                              官公庁・公社・団体
+                              '''),
+                MessageAction(label='②就活ツールについて',
+                              text='test'),
+                MessageAction(label='③インターンについて',
+                              text='test'),
+                MessageAction(label='④OB・OG訪問について',
+                              text='test'),
+                MessageAction(label='⑤その他（感想等）',
+                              text='test')
+            ])
+        template_message = TemplateSendMessage(template=buttons_template)
         line_bot_api.reply_message(event.reply_token, template_message)
+        
+#     if text == 'carousel':
+#         carousel_template = CarouselTemplate(columns=[
+#             CarouselColumn(text='hoge1', title='fuga1', actions=[
+#                 URIAction(label='Go to line.me', uri='https://line.me'),
+#                 PostbackAction(label='ping', data='ping')
+#             ]),
+#             CarouselColumn(text='hoge2', title='fuga2', actions=[
+#                 PostbackAction(label='ping with text', data='ping', text='ping'),
+#                 MessageAction(label='Translate Rice', text='米')
+#             ]),
+#         ])
+#         template_message = TemplateSendMessage(
+#             alt_text='Carousel alt text', template=carousel_template)
+#         line_bot_api.reply_message(event.reply_token, template_message)
         
     else:
         pass
