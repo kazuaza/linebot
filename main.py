6578527ad65a7367c905080585808d55be494105
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 20:59:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -59,6 +59,22 @@ def handle_message(event):
             ])
         template_message = TemplateSendMessage(template=buttons_template)
         line_bot_api.reply_message(event.reply_token, template_message)
+        
+    elif text == 'carousel':
+        carousel_template = CarouselTemplate(columns=[
+            CarouselColumn(text='hoge1', title='fuga1', actions=[
+                URIAction(label='Go to line.me', uri='https://line.me'),
+                PostbackAction(label='ping', data='ping')
+            ]),
+            CarouselColumn(text='hoge2', title='fuga2', actions=[
+                PostbackAction(label='ping with text', data='ping', text='ping'),
+                MessageAction(label='Translate Rice', text='米')
+            ]),
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='Carousel alt text', template=carousel_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+        
     else:
         pass
 
