a9458249905e9b8f4e87646b0d86a993f02fba0b
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 18:12:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=121))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=131))
 #     event.message.text
 
 #     return text
