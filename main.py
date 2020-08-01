24ea69acd6c48a76d2d3212fe2c19aee9f343c71
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 18:14:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=131))
-#     event.message.text
+for i in range(2):
+    def handle_message(event):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 #     return text
 
