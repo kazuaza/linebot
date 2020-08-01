2598e4fba8552058ca9b5e5ab5d74023e52811a3
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 21:33:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,18 +35,26 @@ def callback():
 def handle_message(event):
     text = event.message.text
         
-    if text == 'start':
+#     if text == 'start':
+#         buttons_template = ButtonsTemplate(
+#             title='どの機能を使用しますか？', text='（下記ボタンを押してください）', actions=[
+#                 MessageAction(label='①業界について', text='test'),
+#                 URIAction(label='Go to line.me', uri='https://line.me')
+# #                 MessageAction(label='②就活ツールについて', text='test'),
+# #                 MessageAction(label='③インターンについて', text='test'),
+# #                 MessageAction(label='④OBOG訪問について', text='test'),
+# #                 MessageAction(label='⑤その他（感想等）', text='test')
+#             ])
+#         template_message = TemplateSendMessage(template=buttons_template)
+#         line_bot_api.reply_message(event.reply_token, template_message)
+    if text == 'buttons':
         buttons_template = ButtonsTemplate(
-            title='どの機能を使用しますか？', text='（下記ボタンを押してください）', actions=[
-                MessageAction(label='①業界について', text='test'),
-                URIAction(label='Go to line.me', uri='https://line.me')
-#                 MessageAction(label='②就活ツールについて', text='test'),
-#                 MessageAction(label='③インターンについて', text='test'),
-#                 MessageAction(label='④OBOG訪問について', text='test'),
-#                 MessageAction(label='⑤その他（感想等）', text='test')
+            title='My buttons sample', text='Hello, my buttons', actions=[
+                URIAction(label='Go to line.me', uri='https://line.me'),
+                PostbackAction(label='ping', data='ping'),
+                PostbackAction(label='ping with text', data='ping', text='ping'),
+                MessageAction(label='Translate Rice', text='米')
             ])
-        template_message = TemplateSendMessage(template=buttons_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
 
 
 if __name__ == '__main__':
