f7076345af86e3fe550db53d14e5d44d24335e2d
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 18:54:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -56,8 +56,11 @@ def handle_message(event):
                 URIAction(label='Go to line.me', uri='https://line.me'),
                 PostbackAction(label='ping', data='ping'),
                 PostbackAction(label='ping with text', data='ping', text='ping'),
-                MessageAction(label='Translate Rice', text='米')
-            ])
+                MessageAction(label='Translate Rice', 
+                              text=ButtonsTemplate(title='My buttons sample', text='Hello, my buttons', 
+                                                   actions=[URIAction(label='Go to line.me', uri='https://line.me')])
+                              )
+                ])
         template_message = TemplateSendMessage(
             alt_text='Buttons alt text', template=buttons_template)
         line_bot_api.reply_message(event.reply_token, template_message)
