08c15a35df2a020908a6140adb90e8133cdaaae1
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-01 17:36:08 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 
+num = 0
 app = Flask(__name__)
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ['YOUR_CHANNEL_ACCESS_TOKEN']
@@ -13,6 +14,7 @@ YOUR_CHANNEL_SECRET = os.environ['YOUR_CHANNEL_SECRET']
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
 
 @app.route('/callback', methods=['POST'])
 def callback():
@@ -30,12 +32,29 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == 'python':
-        text = 'Do you like python?'
-    else:
-        text = event.message.text
+    text = event.message.text
+    return text
 
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text))
+
+while True:
+    text = handle_message(MessageEvent)
+    if num == 0 and text == 'start':
+        text1 = '''
+        どの機能を利用しますか？
+        
+        ①業界について
+        ②就活ツールについて
+        ③インターンについて
+        ④OB・OG訪問について
+        ⑤その他（感想等）
+        '''
+        num += 1
+        line_bot_api.reply_message(MessageEvent.reply_token, TextSendMessage(text=text1))
+        break
+    else:
+        text_exception = 'サービス利用の際は start と打ってください'
+        line_bot_api.reply_message(MessageEvent.reply_token, TextSendMessage(text=text_exception))
+        continue
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT'))
