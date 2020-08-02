87103d929b0308bc612bcf211443886d1084c436
kazuaza <49898221+kazuaza@users.noreply.github.com>
2020-08-02 17:07:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -59,14 +59,13 @@ def handle_text_message(event):
 #         template_message = TemplateSendMessage(template=buttons_template)
 #         line_bot_api.reply_message(event.reply_token, template_message)
     
-    if text == 'start':
+    if text == 'buttons':
         buttons_template = ButtonsTemplate(
-            title='どの機能を使用しますか？', text='（下記ボタンを押してください）', actions=[
-                MessageAction(label='①業界について', text='test'),
-                MessageAction(label='②就活ツールについて', text='test'),
-                MessageAction(label='③インターンについて', text='test'),
-                MessageAction(label='④OBOG訪問について', text='test'),
-                MessageAction(label='⑤その他', text='test')
+            title='My buttons sample', text='Hello, my buttons', actions=[
+                URIAction(label='Go to line.me', uri='https://line.me'),
+                PostbackAction(label='ping', data='ping'),
+                PostbackAction(label='ping with text', data='ping', text='ping'),
+                MessageAction(label='Translate Rice', text='米')
             ])
         template_message = TemplateSendMessage(
             alt_text='Buttons alt text', template=buttons_template)
